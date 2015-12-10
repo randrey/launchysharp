a09afaab3a0db71c398743f5f25cb95be3b08992
randrey <andrey.rystenko@hotmail.com>
2015-12-10 01:13:15 -0800

    Build stuff

---
+++ tools/build.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+if "%QTDIR%" == "" (
+  echo 'QTDIR' environment variable not defined!
+  goto :eof
+)
+
+xcopy /y /r /e qt\* %QTDIR%\
+set PATH = %PATH%;%QTDIR%\bin
+pushd
+cd /d %QTDIR%
+configure -confirm-license -opensource -release -platform win32-msvc2005 -no-webkit -no-phonon -no-phonon-backend -no-script -no-scripttools -no-qt3support -no-multimedia -no-ltcg -nomake demos -nomake examples -nomake tools
+nmake
+popd
